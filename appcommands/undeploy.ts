a6cfc4a2c84d2e91aa73ef89c89f8c8a94697f41
net-tech <hello@nettech.link>
2022-08-30 23:49:12 +0200

    feat(commands): add eval and execute + format files

--- appcommands/undeploy.ts
+++ appcommands/undeploy.ts
@@ -1,34 +1,32 @@
-import fs from "fs";
-import path from "path";
-import { SlashCommandBuilder, Routes } from "discord.js";
-import { REST } from "@discordjs/rest";
-require("dotenv").config();
+import fs from "fs"
+import path from "path"
+import { SlashCommandBuilder, Routes } from "discord.js"
+import { REST } from "@discordjs/rest"
+require("dotenv").config()
 
 const undeploy = () => {
-  const commands: any = [];
-  const commandsPath = path.join(__dirname, "../commands");
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".ts"));
+	const commands: any = []
+	const commandsPath = path.join(__dirname, "../commands")
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".ts"))
 
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    commands.push(command.data.toJSON());
-  }
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file)
+		const command = require(filePath)
+		commands.push(command.data.toJSON())
+	}
 
-  const rest = new REST({ version: "10" }).setToken(
-    process.env.TOKEN as string
-  );
+	const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string)
 
-  rest
-    .put(Routes.applicationCommands(process.env.CLIENT_ID as string), {
-      body: [],
-    })
-    .then(() =>
-      console.log("Successfully deregistered global application commands.")
-    )
-    .catch(console.error);
-};
+	rest
+		.put(Routes.applicationCommands(process.env.CLIENT_ID as string), {
+			body: [],
+		})
+		.then(() =>
+			console.log("Successfully deregistered global application commands.")
+		)
+		.catch(console.error)
+}
 
-export default undeploy;
+export default undeploy
--- routes/StaffFileQueryRoutes.ts
+++ routes/StaffFileQueryRoutes.ts
@@ -1,99 +1,145 @@
 import { QueryTypes } from "sequelize"
-import { dbSql } from "..";
-import { StaffFile } from "../types/common/ReturnTypes";
+import { dbSql } from ".."
+import { StaffFile } from "../types/common/ReturnTypes"
 
 /**
  * Provides easy accessible query routes for the "stafffiles" table of the database.
  */
 export default class StaffFileQueryRoutes {
-    /**
-     * Queries for a staff member by full name and returns their StaffFile.
-     * @param firstName The logged first name of the user.
-     * @param lastName The logged last name of the user.
-     * @returns Staff File Object
-     */
-    async getStaffByFullName(firstName: string, lastName: string): Promise<StaffFile> {
-        return (
-            await dbSql.query(`SELECT * FROM stafffiles WHERE name = '${firstName} ${lastName}'`, { type: QueryTypes.SELECT })
-        )[0] as StaffFile
-    }
-    /**
-     * Queries for a staff member(s) by only their first name. Returns an array of StaffFiles which can be handled, iterated, etc.
-     * @param name The logged name to query. Must be first name.
-     * @returns Array of Staff File Objects
-     */
-    async getStaffByFirstName(name: string): Promise<object[]> {
-        return dbSql.query(`SELECT * FROM stafffiles WHERE name LIKE '${name} %'`, { type: QueryTypes.SELECT })
-    }
-    /**
-     * Queries for a staff member(s) by only their last name. Returns an array of StaffFiles which can be handled, iterated, etc.
-     * @param name The logged name to query. Must be last name.
-     * @returns Array of Staff File Objects
-     */
-    async getStaffByLastName(name: string): Promise<object[]> {
-        return dbSql.query(`SELECT * FROM stafffiles WHERE name LIKE '%${name}'`, { type: QueryTypes.SELECT })
-    }
-    /**
-     * Queries for a staff member by their unique Primary Key. Returns a Staff File Object.
-     * @param id Unique Primary Key.
-     * @returns Staff File Object
-     */
-    async getStaffById(id: number): Promise<object> {
-        return (
-            await dbSql.query(`SELECT * FROM stafffiles WHERE id = ${id}`, { type: QueryTypes.SELECT })
-        )[0]
-    }
-    /**
-     * Queries for a staff member by their personal or company email. Returns an array of StaffFiles which can be handled, iterated, etc.
-     * @param email The address to match to the system.
-     * @param type The type of the email, either "Personal" or "Company" (@schoolsimplified.org).
-     * @returns Array of Staff File Objects
-     */
-    async getStaffByEmail(email: string, type: "Personal" | "Company"): Promise<object[]> {
-        if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) throw new Error("getStaffByEmail - email parameter must be an email address.")
-        if (type == "Personal") {
-            return dbSql.query(`SELECT * FROM stafffiles WHERE personalEmail = '${email}'`, { type: QueryTypes.SELECT })
-        } else return dbSql.query(`SELECT * FROM stafffiles WHERE companyEmail = '${email}'`, { type: QueryTypes.SELECT })
-    }
-    /**
-     * Queries for a staff team via an intermediate Staff File. Returns the Team file.
-     * @param staff The Staff File to use.
-     * @returns Team Object.
-     */
-    async getTeamByStaffTeam(staff: object): Promise<object> {
-        return dbSql.query(`SELECT * FROM teams WHERE id = (SELECT TeamId FROM stafffiles WHERE id = ${(staff as StaffFile).id})`) // TODO
-    }
-    /**
-     * Queries for a staff department via an intermediate Staff File. Returns the Department file.
-     * @param staff The Staff File to use.
-     * @returns Department Object.
-     */
-    async getDepartmentByStaffDepartment(staff: object): Promise<object> {
-        return dbSql.query(`SELECT * FROM departments WHERE id = (SELECT DepartmentId FROM stafffiles WHERE id = ${(staff as StaffFile).id})`)
-    }
-    /**
-     * Queries for a staff position via an intermediate Staff File. Returns the Position file.
-     * @param staff The Staff File to use.
-     * @returns Position Object.
-     */
-    async getPositionByStaffPosition(staff: object): Promise<object> {
-        return dbSql.query(`SELECT * FROM positions WHERE id = (SELECT PositionId FROM stafffiles WHERE id = ${(staff as StaffFile).id})`)
-    }
-    /**
-     * Initializes a Staff File, Position History, and Position Staff link entry with all required staff details.
-     * @param fullName The first and last name of the user.
-     * @param activityStatus Choose from a list of valid activity statuses.
-     * @param departmentName The department the user is joining.
-     * @param positionName The position the user is being onboarded in.
-     * @param email The company email of the user.
-     * @param teamName Optional, the name of the team being joined.
-     * @param appStatus Optional, the updated application status of the user.
-     * @returns New Staff File, Position History, and Position Staff entries.
-     */
-    async createStaffFile(fullName: string, activityStatus: "Active" | "On Break" | "Resigned" | "Fired" | "Blacklisted", departmentName: string, positionName: string, email: string, teamName?: string, appStatus?: "Awaiting Review" | "Rejected" | "Awaiting Response" | "Awaiting Onboarding" | "Awaiting Server Entry" | "Onboarding Phase"): Promise<void> {
-        if (appStatus) {
-            if (teamName) {
-                dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
+	/**
+	 * Queries for a staff member by full name and returns their StaffFile.
+	 * @param firstName The logged first name of the user.
+	 * @param lastName The logged last name of the user.
+	 * @returns Staff File Object
+	 */
+	async getStaffByFullName(
+		firstName: string,
+		lastName: string
+	): Promise<StaffFile> {
+		return (
+			await dbSql.query(
+				`SELECT * FROM stafffiles WHERE name = '${firstName} ${lastName}'`,
+				{ type: QueryTypes.SELECT }
+			)
+		)[0] as StaffFile
+	}
+	/**
+	 * Queries for a staff member(s) by only their first name. Returns an array of StaffFiles which can be handled, iterated, etc.
+	 * @param name The logged name to query. Must be first name.
+	 * @returns Array of Staff File Objects
+	 */
+	async getStaffByFirstName(name: string): Promise<object[]> {
+		return dbSql.query(`SELECT * FROM stafffiles WHERE name LIKE '${name} %'`, {
+			type: QueryTypes.SELECT,
+		})
+	}
+	/**
+	 * Queries for a staff member(s) by only their last name. Returns an array of StaffFiles which can be handled, iterated, etc.
+	 * @param name The logged name to query. Must be last name.
+	 * @returns Array of Staff File Objects
+	 */
+	async getStaffByLastName(name: string): Promise<object[]> {
+		return dbSql.query(`SELECT * FROM stafffiles WHERE name LIKE '%${name}'`, {
+			type: QueryTypes.SELECT,
+		})
+	}
+	/**
+	 * Queries for a staff member by their unique Primary Key. Returns a Staff File Object.
+	 * @param id Unique Primary Key.
+	 * @returns Staff File Object
+	 */
+	async getStaffById(id: number): Promise<object> {
+		return (
+			await dbSql.query(`SELECT * FROM stafffiles WHERE id = ${id}`, {
+				type: QueryTypes.SELECT,
+			})
+		)[0]
+	}
+	/**
+	 * Queries for a staff member by their personal or company email. Returns an array of StaffFiles which can be handled, iterated, etc.
+	 * @param email The address to match to the system.
+	 * @param type The type of the email, either "Personal" or "Company" (@schoolsimplified.org).
+	 * @returns Array of Staff File Objects
+	 */
+	async getStaffByEmail(
+		email: string,
+		type: "Personal" | "Company"
+	): Promise<object[]> {
+		if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))
+			throw new Error(
+				"getStaffByEmail - email parameter must be an email address."
+			)
+		if (type == "Personal") {
+			return dbSql.query(
+				`SELECT * FROM stafffiles WHERE personalEmail = '${email}'`,
+				{ type: QueryTypes.SELECT }
+			)
+		} else
+			return dbSql.query(
+				`SELECT * FROM stafffiles WHERE companyEmail = '${email}'`,
+				{ type: QueryTypes.SELECT }
+			)
+	}
+	/**
+	 * Queries for a staff team via an intermediate Staff File. Returns the Team file.
+	 * @param staff The Staff File to use.
+	 * @returns Team Object.
+	 */
+	async getTeamByStaffTeam(staff: object): Promise<object> {
+		return dbSql.query(
+			`SELECT * FROM teams WHERE id = (SELECT TeamId FROM stafffiles WHERE id = ${
+				(staff as StaffFile).id
+			})`
+		) // TODO
+	}
+	/**
+	 * Queries for a staff department via an intermediate Staff File. Returns the Department file.
+	 * @param staff The Staff File to use.
+	 * @returns Department Object.
+	 */
+	async getDepartmentByStaffDepartment(staff: object): Promise<object> {
+		return dbSql.query(
+			`SELECT * FROM departments WHERE id = (SELECT DepartmentId FROM stafffiles WHERE id = ${
+				(staff as StaffFile).id
+			})`
+		)
+	}
+	/**
+	 * Queries for a staff position via an intermediate Staff File. Returns the Position file.
+	 * @param staff The Staff File to use.
+	 * @returns Position Object.
+	 */
+	async getPositionByStaffPosition(staff: object): Promise<object> {
+		return dbSql.query(
+			`SELECT * FROM positions WHERE id = (SELECT PositionId FROM stafffiles WHERE id = ${
+				(staff as StaffFile).id
+			})`
+		)
+	}
+
+	async createStaffFile(
+		fullName: string,
+		activityStatus:
+			| "Active"
+			| "On Break"
+			| "Resigned"
+			| "Fired"
+			| "Blacklisted",
+		departmentName: string,
+		positionName: string,
+		email: string,
+		teamName?: string,
+		appStatus?:
+			| "Awaiting Review"
+			| "Rejected"
+			| "Awaiting Response"
+			| "Awaiting Onboarding"
+			| "Awaiting Server Entry"
+			| "Onboarding Phase"
+	): Promise<object> {
+		if (appStatus) {
+			if (teamName) {
+				return dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
                 SET @positionid = (SELECT id FROM positions WHERE title = '${positionName}');
                 SET @teamid = (SELECT id FROM teams WHERE name = '${teamName}');
                 INSERT INTO stafffiles (
@@ -115,9 +161,8 @@ export default class StaffFileQueryRoutes {
                 VALUES (
                     now(), now(), @staffid, @positionid
                 )`)
-                return
-            } else {
-                dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
+			} else {
+				return dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
                 SET @positionid = (SELECT id FROM positions WHERE title = '${positionName}');
                 INSERT INTO stafffiles (
                     name, personalEmail, companyEmail, photoLink, phone, legalSex, genderIdentity, ethnicity, appStatus, strikes, censures, pips, activityStatus, alumni, createdAt, updatedAt, DepartmentId, PositionId
@@ -138,11 +183,10 @@ export default class StaffFileQueryRoutes {
                 VALUES (
                     now(), now(), @staffid, @positionid
                 )`)
-                return
-            }
-        } else {
-            if (teamName) {
-                dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
+			}
+		} else {
+			if (teamName) {
+				return dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
                 SET @positionid = (SELECT id FROM positions WHERE title = '${positionName}');
                 SET @teamid = (SELECT id FROM teams WHERE name = '${teamName}');
                 INSERT INTO stafffiles (
@@ -164,9 +208,8 @@ export default class StaffFileQueryRoutes {
                 VALUES (
                     now(), now(), @staffid, @positionid
                 )`)
-                return
-            } else {
-                dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
+			} else {
+				return dbSql.query(`SET @departmentid = (SELECT id FROM departments WHERE name = '${departmentName}');
                 SET @positionid = (SELECT id FROM positions WHERE title = '${positionName}');
                 INSERT INTO stafffiles (
                     name, personalEmail, companyEmail, photoLink, phone, legalSex, genderIdentity, ethnicity, strikes, censures, pips, activityStatus, alumni, createdAt, updatedAt, DepartmentId, PositionId
@@ -187,21 +230,13 @@ export default class StaffFileQueryRoutes {
                 VALUES (
                     now(), now(), @staffid, @positionid
                 )`)
-                return
-            }
-        }
-    }
-    /**
-     * Assigns a designated, existing user as a Supervisor.
-     * @param fullName The full name of the user.
-     * @param title The title of the user. This position must be assigned as a Supervisor role or this will have no effect.
-     * @returns Updated Supervisor entry.
-     */
-    async assignSupervisor(fullName: string, title: string): Promise<void> {
-        dbSql.query(`UPDATE supervisors
+			}
+		}
+	}
+
+	async assignSupervisor(fullName: string, title: string) {
+		return dbSql.query(`UPDATE supervisors
         SET StaffFileId = (SELECT id FROM stafffiles WHERE name = '${fullName}')
         WHERE title = '${title}'`)
-        return
-    }
-
+	}
 }
