22aa2e5da5aebb492b957182939adde04beecd2e
tm21cy <tm21cy@brocku.ca>
2022-09-01 18:08:12 -0400

    Minor API updates

--- index.ts
+++ index.ts
@@ -419,11 +419,7 @@ app.use(cors({
 	origin: "*"
 }))
 app.use(morgan('combined'))
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-  });
+
 
 app.get('/', (req: any, res: any) => {
 	res.send(src)
