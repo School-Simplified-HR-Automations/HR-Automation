f1213d2e312a27d24a05962e08b2f7f1a0ba90dc
tm21cy <tm21cy@brocku.ca>
2022-10-11 23:58:40 -0400

    feat(utils): add v8 utility methods

---
+++ utils/sanitizer.ts
@@ -0,0 +1,55 @@
+export default function sanitizer(type: "number" | "StaffFileId" | "name" | "discordId" | "timestamp" | "discordUser" | "date" | "link", ...args: string[]): void {
+    let pass = true
+    switch (type) {
+        case "number":
+            const numreg = new RegExp("^[0-9]+$", "gm")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(numreg)) pass = false
+            }
+            break;
+        case "StaffFileId":
+            const sfireg = new RegExp("^[0-9]{1,6}+$", "gm")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(sfireg)) pass = false
+            }
+            break;
+        case "name":
+            const namereg = new RegExp("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", "gmu")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(namereg)) pass = false
+            }
+            break;
+        case "discordId":
+            const direg = new RegExp(/[0-9]{17,20}/, "gm")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(direg)) pass = false
+            }
+            break;
+        case "timestamp":
+            const tsreg = new RegExp(/[0-9]{10,13}/, "gm")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(tsreg)) pass = false
+            }
+            break;
+        case "discordUser":
+            const dureg = new RegExp("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+#[0-9]{4}$", "gmu")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(dureg)) pass = false
+            }
+            break;
+        case "date":
+            const datereg1 = new RegExp(/[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/)
+            const datereg2 = new RegExp(/[0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}/)
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(datereg1) && !args[i].match(datereg2)) pass = false
+            }
+            break;
+        case "link":
+            const linkreg = new RegExp(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/, "gm")
+            for (let i = 0; i < args.length; i++) {
+                if (!args[i].match(linkreg)) pass = false
+            }
+            break;
+    }
+    if (!pass) throw new Error("Security: Input did not pass sanitization.")
+}
--- utils/sendError.ts
+++ utils/sendError.ts
@@ -4,6 +4,8 @@ import Discord, {
 	Message,
 	EmbedBuilder,
 	ChatInputCommandInteraction,
+	ModalSubmitInteraction,
+	SelectMenuInteraction,
 } from "discord.js"
 
 const sendError = (
@@ -12,7 +14,9 @@ const sendError = (
 		| CommandInteraction
 		| ButtonInteraction
 		| Message
-		| ChatInputCommandInteraction,
+		| ChatInputCommandInteraction
+		| ModalSubmitInteraction
+		| SelectMenuInteraction,
 	def?: boolean
 ) => {
 	const errorEmbed = new EmbedBuilder()
@@ -21,6 +25,9 @@ const sendError = (
 			`Oops! It appears the bot encountered an error while handling that request. No worries, you can continue to use the bot as normal - the log is attached for you to send to the Support team.\n\nError:\n\`\`\`${e}\`\`\``
 		)
 		.setColor("Red")
+		if (type instanceof ModalSubmitInteraction) {
+			return type.reply({ embeds: [errorEmbed] })
+		}
 		if (def && type instanceof ChatInputCommandInteraction) {
 			return type.editReply({ embeds: [errorEmbed] })
 		}
